a3f622f4f7778737153667fdaa086e6f14d6d65f
CirclePE <lwh8762@naver.com>
2016-08-28 20:59:04 +0900

    Test message send

--- app/src/main/java/com/lwh8762/katalkutil/MainActivity.java
+++ app/src/main/java/com/lwh8762/katalkutil/MainActivity.java
@@ -3,11 +3,27 @@ package com.lwh8762.katalkutil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.kakao.KakaoLink;
+import com.kakao.KakaoParameterException;
+import com.kakao.KakaoTalkLinkMessageBuilder;
+
 public class MainActivity extends AppCompatActivity {
+    private KakaoLink kakaoLink;
+    private KakaoTalkLinkMessageBuilder kakaoTalkLinkMessageBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            kakaoLink = KakaoLink.getKakaoLink(this);
+            kakaoTalkLinkMessageBuilder = kakaoLink.createKakaoTalkLinkMessageBuilder();
+            kakaoTalkLinkMessageBuilder.addText("TEST");
+            kakaoLink.sendMessage(kakaoTalkLinkMessageBuilder.build(),this);
+        } catch (KakaoParameterException e) {
+            e.printStackTrace();
+        }
+
     }
 }
